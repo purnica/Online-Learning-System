1014ad2bbf4b882d8242bbd5ddf08f10bf00f268
purnica <prajapatipurnika26@gmail.com>
2024-11-27 21:10:39 +0545

    about-section added

---
+++ Register/login.js
@@ -0,0 +1,33 @@
+function validate(){
+    let email = document.getElementById("e").value;
+    let password = document.getElementById("p").value;
+
+    if (email == "") {
+        alert("please provide your email");
+        document.getElementById("e").focus();
+        return false;
+    }
+
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/;
+    if(!emailPattern.test(email)){
+        alert("Please enter a valid email address!");
+        document.getElementById("e").focus();
+        return false;
+    }
+
+    if (password == "") {
+        alert("please provide your password");
+        document.getElementById("p").focus();
+        return false;
+    }
+
+    var passwordPattern= /^.{6,9}$/;
+    if(!passwordPattern.test(password)){
+        alert("Enter a password with at least 6 character");
+        document.getElementById("p").focus();
+        return false;
+    }
+
+    alert("Login succesfully");
+    return true;
+}
