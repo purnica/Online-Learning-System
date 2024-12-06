ea5ba5060aeff037584731db7584e8a3d7ab6924
purnica <prajapatipurnika26@gmail.com>
2024-12-06 19:34:04 +0545

    admin php added

---
+++ PHP/admindb.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "Mysql@123";
+$dbname = "project";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if (!$conn){
+    die ("connection failed".mysqli_connect_error());
+}
+
+// echo "Connection success"."<br>";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $email = $_POST['e'];
+    $pass = $_POST['pass'];
+
+$sql="select * from admin where email='$email' AND password='$pass'";
+
+$result= mysqli_query($conn,$sql);
+
+$num=mysqli_num_rows($result);
+if($num == 1 or $num == 2)
+{
+header("Location:webPage.php");
+exit;
+}
+else{
+echo "invalid username and password";
+}
+}
+
+mysqli_close($conn);
+?>
